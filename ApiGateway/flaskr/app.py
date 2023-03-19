21698225b19f2e9ad96c8fd9deabc1c58f429b54
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2023-03-18 22:23:33 -0500

    Llamado servcio solicitar token desde ApiGateway

--- ApiGateway/flaskr/app.py
+++ ApiGateway/flaskr/app.py
@@ -1,7 +1,7 @@
 from flaskr import create_app
 
 from flask_restful import Api
-from .vistas import VistaAPIGenerarOrdenVenta
+from .vistas import VistaAPIGenerarOrdenVenta, VistaAPISolicitarToken
 
 app = create_app('default')
 app_context= app.app_context()
@@ -9,3 +9,4 @@ app_context.push()
 
 api=Api(app)
 api.add_resource(VistaAPIGenerarOrdenVenta,'/orden/generar')
+api.add_resource(VistaAPISolicitarToken,'/token/generar')
